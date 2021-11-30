eb6bc1210bd968b28639aae24fc4fe1c74f8820f
Arun <arun_kumar2@mcafee.com>
2021-11-30 09:47:26 +0530

    initial commit

---
+++ app/src/main/java/com/mcafee/basicscriptapplication/SampleClass.java
@@ -0,0 +1,14 @@
+package com.mcafee.basicscriptapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SampleClass extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+
+}
